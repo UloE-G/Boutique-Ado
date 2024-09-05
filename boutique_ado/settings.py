15c81011414077cc2c5631849bcee32e8ab3320f
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-05 11:02:01 +0000

    Removed middleware

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
